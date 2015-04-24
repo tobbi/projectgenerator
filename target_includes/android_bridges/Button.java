b8bf139ab30ac2de86057ce603084c51d9af02cc
Tobias Markus <tobbi.bugs@googlemail.com>
2015-04-24 13:45:34 +0200

    Unused include compiler warning

--- target_includes/android_bridges/Button.java
+++ target_includes/android_bridges/Button.java
@@ -1,7 +1,6 @@
 package com.fhflensburg.tobiasmarkus.androidBridge;
 
 import android.content.Context;
-import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 
