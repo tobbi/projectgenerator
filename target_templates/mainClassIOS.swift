cc707712c344ba3f8f96e606c5340abe0b53c7b1
Tobias Markus <tobbi.bugs@googlemail.com>
2015-04-26 09:59:15 +0200

    Add target_templates

---
+++ target_templates/mainClassIOS.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MobileApplicationsSwiftAufgabe1
+//
+//  Created by Tobias Markus on 02.02.15.
+//  Copyright (c) 2015 Tobias Markus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    //@classContext
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        //@mainContext
+        return true
+    }
+}
